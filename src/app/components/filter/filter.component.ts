0e8688638d632b3117c67eaf61645a3d5e7e7ef7
gisman23 <gis@theshuberthome.com>
2024-12-29 18:48:38 -0500

    filter scroll

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonList, IonItem,  IonAccordion, IonAccordionGroup, IonLabel, IonTitle, 
-   IonHeader, IonContent } from '@ionic/angular/standalone';
+import {
+  IonList, IonItem, IonLabel, IonTitle,
+  IonHeader, IonContent
+} from '@ionic/angular/standalone';
 import { DataBaseService } from 'src/app/services/Database.service';
 import { ModalController } from '@ionic/angular/standalone';
 
@@ -11,8 +13,8 @@ import { ModalController } from '@ionic/angular/standalone';
   templateUrl: 'filter.component.html',
   styleUrls: ['filter.component.css'],
   standalone: true,
-  imports: [IonHeader, IonTitle, IonLabel, IonAccordionGroup, IonAccordion, 
-     IonItem, IonList, CommonModule,  IonContent],
+  imports: [IonHeader, IonTitle, IonLabel,
+    IonItem, IonList, CommonModule, IonContent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FilterComponent {
@@ -20,25 +22,25 @@ export class FilterComponent {
   viewCtrl = inject(ModalController)
 
   species: any
-  fishermen:any
+  fishermen: any
 
-  
-  constructor(){
+
+  constructor() {
     this.getSpecies()
     this.getFishermen()
   }
 
-  async getSpecies(){
+  async getSpecies() {
     await this.dataService.getSpecies()
     this.species = this.dataService.species().sort()
   }
 
-  async getFishermen(){
+  async getFishermen() {
     await this.dataService.getFishermen()
     this.fishermen = this.dataService.fishermen().sort()
   }
 
-  dismiss(){
+  dismiss() {
     this.viewCtrl.dismiss();
-}
+  }
 }
